54efc9024a05c3a0ae406ebba790e9af10170845
tangcheng <tangcheng_java@foxmail.com>
2023-04-14 20:23:15 +0800

    变更

---
+++ server/src/main/java/com/minibyte/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.minibyte.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    private Logger logger = LoggerFactory.getLogger(TestController.class);
+}
---
+++ server/src/main/java/com/minibyte/service/FileIndexWriterService.java
@@ -0,0 +1,15 @@
+package com.minibyte.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 文件索引服务
+ *
+ * @author: tangcheng_cd
+ * @date: 2023/4/14
+ * @description:
+ */
+@Service
+public class FileIndexWriterService {
+
+}
