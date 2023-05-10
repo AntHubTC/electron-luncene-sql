28e7accb228f61e4044b2afc0cd8001efb4d9e1b
anthubtc <1096648786@qq.com>
2023-05-11 00:24:23 +0800

    前端接入后端搜索服务

--- my-electron-app/renderer/render.js
+++ my-electron-app/renderer/render.js
@@ -1,5 +1,61 @@
-const { ipcRenderer } = require("electron");
+// const { ipcRenderer } = require("electron");
 
-ipcRenderer.on("open-img", (event, filePath) => {
-    document.getElementById("gmm").setAttribute("src", filePath);
+// ipcRenderer.on("open-img", (event, filePath) => {
+//     // document.getElementById("gmm").setAttribute("src", filePath);
+// })
+
+pageReady(function () {
+    new Vue({
+        el: '#app',
+        data () {
+            return {
+                options: [
+                    {
+                        label: "sql内容",
+                        value: 4
+                    },
+                    {
+                        label: "按sql名称",
+                        value: 1
+                    },
+                    {
+                        label: "文件名",
+                        value: 2
+                    },
+                    {
+                        label: "作者",
+                        value: 3
+                    }
+                ],
+                conditionType: 4,
+                conditionValue: "",
+                dataList: []
+            }
+        },
+        methods: {
+            search() {
+                axios.post('http://localhost:8080/fileIndex/search',{
+                    conditions: [
+                        {
+                            type: this.conditionType,
+                            value: this.conditionValue
+                        }
+                    ]
+                }).then((response) => {
+                    if (response.status === 200) {
+                        let resData = response.data;
+                        if (resData.status === "000000") {
+                            this.dataList = resData.data;
+                            this.dataList.forEach(dataItem => {
+                                dataItem.content = dataItem.content.replaceAll("\n", "<br/>");
+                            });
+                        }
+                    }
+                });
+            }
+        },
+        created () {
+        
+        }
+    })
 })
